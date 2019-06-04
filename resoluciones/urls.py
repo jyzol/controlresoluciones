03c4ab9cfad6a7f5b46a9c2e4ced3644e339e0a7
Josue Tito <jtitocarpio@gmail.com>
2019-06-04 02:02:57 -0500

    Conexion entre vista, template y modelo Resolucion

--- resoluciones/urls.py
+++ resoluciones/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import home
+from .views import home, resolucion_list
 
 urlpatterns = [
-    path('home', home),
+    path('home', resolucion_list),
 ]
--- resoluciones/views.py
+++ resoluciones/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from resoluciones.models import Resolucion
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'resolucion/index.html')
+    return render(request, 'resolucion/index.html',
+                  {'resoluciones': Resolucion.objects.count(),
+                   'dato': 22})
+
+
+def resolucion_list(request):
+    #resoluciones = Resolucion.objects.all()
+    #resoluciones = Resolucion._meta.model.objects.all()
+    resoluciones = Resolucion.objects.filter(
+        tipo_elevacion='Conocimiento'
+    )
+    return render(request, 'resolucion/index.html',
+                  {'resoluciones': resoluciones})
+
 
 
 
